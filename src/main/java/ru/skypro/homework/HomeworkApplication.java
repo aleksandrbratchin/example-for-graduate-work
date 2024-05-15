83ddeb2c4ac88d39cd4534d0afdeb5089b2f65b8
aleksbrat <aleksbrat@gmail.com>
2024-05-15 16:05:07 +0300

    springdoc-openapi-ui только дукументация

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -1,12 +1,13 @@
 package ru.skypro.homework;
 
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableSwagger2
+@OpenAPIDefinition
 public class HomeworkApplication {
   public static void main(String[] args) {
     SpringApplication.run(HomeworkApplication.class, args);
