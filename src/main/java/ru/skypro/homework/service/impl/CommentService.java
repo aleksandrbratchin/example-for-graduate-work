d4e29c7456d3f92f10bb6b66ee6365db663b011a
Надежда <nadin94403@gmail.com>
2024-05-27 20:08:27 +0300

    скопировала

--- src/main/java/ru/skypro/homework/service/impl/CommentService.java
+++ src/main/java/ru/skypro/homework/service/impl/CommentService.java
@@ -2,13 +2,24 @@ package ru.skypro.homework.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.response.CommentResponse;
+import ru.skypro.homework.model.Comment;
+import ru.skypro.homework.repository.AdRepository;
 import ru.skypro.homework.repository.CommentRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
+
+    private final AdRepository adRepository;
+
+//    public List<CommentResponse> getComments (long id){
+//       List <Comment> list = adRepository.f;
+//    }
 
 
 }
