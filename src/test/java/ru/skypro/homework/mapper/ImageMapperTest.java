0ce9f143d6f0e613ca966e0571053892f3a7924c
aleks <alexbratchin@gmail.com>
2024-05-27 11:47:37 +0300

    Тесты мапперов для Image и User

---
+++ src/test/java/ru/skypro/homework/mapper/ImageMapperTest.java
@@ -0,0 +1,45 @@
+package ru.skypro.homework.mapper;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.model.Image;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ImageMapperTest {
+
+    private final ImageMapper imageMapper = Mappers.getMapper(ImageMapper.class);
+
+    @Test
+    @SneakyThrows
+    public void testToImage() {
+        byte[] fileContent = {1, 2, 3, 4};
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "test.png",
+                "image/png",
+                fileContent
+        );
+
+        Image image = imageMapper.toImage(multipartFile);
+
+        assertThat(image).isNotNull();
+        assertThat(image.getFileSize()).isEqualTo(multipartFile.getSize());
+        assertThat(image.getMediaType()).isEqualTo(multipartFile.getContentType());
+        assertThat(image.getData()).isEqualTo(multipartFile.getBytes());
+        assertThat(image.getId()).isNull();
+    }
+
+    @Test
+    public void testToImageWithNullFile() {
+        MultipartFile nullFile = null;
+
+        Image image = imageMapper.toImage(nullFile);
+
+        assertThat(image).isNull();
+    }
+
+}
