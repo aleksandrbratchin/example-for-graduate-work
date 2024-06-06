7bcbb4129c5bdeb6069eb38d905e2aabae234bb3
Надежда <nadin94403@gmail.com>
2024-06-06 22:46:05 +0300

    добавлено логирование в комментариях

--- src/main/java/ru/skypro/homework/service/impl/CommentService.java
+++ src/main/java/ru/skypro/homework/service/impl/CommentService.java
@@ -2,6 +2,7 @@ package ru.skypro.homework.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.CreateOrUpdateComment;
 import ru.skypro.homework.exception.CommentNotFoundException;
@@ -14,6 +15,7 @@ import ru.skypro.homework.service.CommentServiceApi;
 import java.util.List;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class CommentService implements CommentServiceApi {
 
@@ -24,6 +26,12 @@ public class CommentService implements CommentServiceApi {
     @Override
     @Transactional
     public Comment addCommentToAd(Long adId, Comment newComment) {
+        log.info("Добавление комментария: {}", newComment.getId() +
+                ", " + newComment.getText() +
+                ", " + newComment.getUser() +
+                ", " + newComment.getCreatedAt() +
+                " к объявлению " + adId
+        );
         Comment save = commentRepository.save(newComment);
         Ad ad = adService.getAdById(adId);
         ad.getComments().add(save);
@@ -34,6 +42,7 @@ public class CommentService implements CommentServiceApi {
     @Override
     @Transactional
     public void deleteCommentFromAd(Long adId, Long commentId) {
+        log.info("Удаление комментария с id: {}", commentId + ", объявления с id " + adId);
         Ad ad = adService.getAdById(adId);
         Comment comment = getCommentFromAdById(ad, commentId);
         ad.getComments().remove(comment);
@@ -47,6 +56,7 @@ public class CommentService implements CommentServiceApi {
             Long commentId,
             CreateOrUpdateComment createOrUpdateComment
     ) {
+        log.info("Редактирование комментария с id: {}", commentId + ", объявления с id " + adId);
         Ad ad = adService.getAdById(adId);
         Comment comment = getCommentFromAdById(ad, commentId);
         comment.setText(createOrUpdateComment.getText());
@@ -55,10 +65,12 @@ public class CommentService implements CommentServiceApi {
 
     @Override
     public List<Comment> getCommentsForAd(Long id) {
+        log.info("Получение всех комментариев объявления: {}", id);
         return adService.getAdById(id).getComments();
     }
 
     private Comment getCommentFromAdById(Ad ad, Long commentId) {
+        log.info("Получение комментария с id: {}", commentId + ", объявления с id " + ad.getId());
         return ad.getComments().stream()
                 .filter(comment -> comment.getId().equals(commentId))
                 .findFirst()
@@ -68,6 +80,7 @@ public class CommentService implements CommentServiceApi {
 
     @Override
     public Comment getCommentById(Long id) {
+        log.info("Получение комментария по id: {}", id);
         return commentRepository.findById(id).orElseThrow(
                 () -> new CommentNotFoundException("Комментария с id=" + id + " не найдено")
         );
